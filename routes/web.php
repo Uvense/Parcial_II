ced225ad7b10e470d30a23fe26cfa016215880d7
Uvense <oscaruvense@gmai.com>
2023-04-18 10:24:40 -0600

    modificacion del archivo PedidosController y web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use  App\Http\PedidosController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+Route::resource('pedidos',PedidosController::class);
 });
