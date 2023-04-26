834ec5c941f8baba46f1877fdb0f2abcb2cb680c
Uvense <oscaruvense@gmail.com>
2023-04-26 02:23:30 -0600

    modelo

---
+++ parcial/app/Models/Pedido.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido extends Model
+{
+    use HasFactory;
+    protected $table = 'pedidos';
+
+    protected $fillable = [
+        'fecha_de_pedido',
+        'estado_del_pedido',
+        'total',
+    ];
+    
+}
